6e11f24f7a4495a7edc15e115c0ee934221f790d
Flávio Eduardo <xflax00@gmail.com>
2023-05-25 23:01:24 -0300

    feat: added react-router-dom and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
+
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home.tsx";
+import Cart from "./pages/Cart/Cart.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <Box minH="100vh" minW="100vw" className="body-container">
-        <App />
-      </Box>
+      <BrowserRouter>
+        <Box minH="100vh" minW="100vw" className="body-container">
+          <Routes>
+            <Route path="/react-cart" element={<Home />} />
+            <Route path="/react-cart/cart" element={<Cart />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
