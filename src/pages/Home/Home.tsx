379deb3ec16724d101c302416aef5ecf1856478d
Flávio Eduardo <xflax00@gmail.com>
2023-05-31 20:45:12 -0300

    fix: item alignment styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -43,6 +43,36 @@ const shoes = [
     name: "Nike Air Force 1 08 Black",
     price: 999.99,
     imageSrc: "https://static.dafiti.com.br/p/Nike-T%C3%AAnis-Nike-Air-Force-1-'07-Masculino-6736-31652411-1-zoom.jpg",
+  },
+  {
+    id: 7,
+    name: "Nike Shoe White",
+    price: 300,
+    imageSrc: "https://imgnike-a.akamaihd.net/1920x1920/02362651.jpg",
+  },
+  {
+    id: 8,
+    name: "Nike Air Force 1 07 Black",
+    price: 799.99,
+    imageSrc: "https://static.dafiti.com.br/p/Nike-T%C3%AAnis-Nike-Air-Force-1-'07-Masculino-6736-31652411-1-zoom.jpg",
+  },
+  {
+    id: 9,
+    name: "Tênis Nike Air Force 1 Low Retro",
+    price: 809.99,
+    imageSrc: "https://authenticfeet.vteximg.com.br/arquivos/ids/259087-1000-1000/Tenis-Nike-Air-Force-1-Low-Retro-Branco-1.jpg?v=637897659401200000",
+  },
+  {
+    id: 10,
+    name: "Tênis Nike Air Force 1'07",
+    price: 899.99,
+    imageSrc: "https://imgnike-a.akamaihd.net/1300x1300/023215BP.jpg",
+  },
+  {
+    id: 11,
+    name: "Nike Air Force 1 Low",
+    price: 600,
+    imageSrc: "https://storage.googleapis.com/ssr_br/products/64d50114f7909efc20eaf24f00b52961.jpg",
   }
 ]
 
