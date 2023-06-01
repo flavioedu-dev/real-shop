48af6544e5c1a0f56b0c91ab78121eaac6fc7e58
Flávio Eduardo <xflax00@gmail.com>
2023-05-31 23:07:46 -0300

    feat: added items cart and style of it

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,8 @@ import styles from "./Header.module.css"
 // Components
 import PrimaryInput from "../PrimaryInput/PrimaryInput"
 
+import cartItems from "../../assets/cart-items.png"
+
 import { ChangeEvent, useState } from "react"
 
 const Header = () => {
@@ -19,7 +21,10 @@ const Header = () => {
     <header className={styles.Header}>
       <span>Realshop</span>
       <PrimaryInput type="text" name="search-item" placeholder="Buscar produto..." onchange={handleSearch} value={searchItem} />
-      <div></div>
+      <div className={styles.cart_nav}>
+        <img src={cartItems} alt="cart-items" />
+        <span>{13}</span>
+      </div>
     </header>
   )
 }
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -2,7 +2,7 @@ import styles from "./Item.module.css"
 
 import { Card, CardBody, CardFooter, Image, Heading, Divider } from "@chakra-ui/react"
 
-import cart from "../../assets/cart-outline.png"
+import cart from "../../assets/cart-add.png"
 
 interface ItemProps {
   imageSrc: string;
@@ -25,13 +25,12 @@ const Item = ({ imageSrc, imageAlt, price, description, isActive, onshow, unshow
           </CardBody>
           <Divider />
           <CardFooter display="flex" flexDirection="column">
-            <Heading className={styles.price} fontSize="1.5rem" fontWeight={500}>R${price} <span>x10 sem juros</span></Heading>
+            <Heading className={styles.price} fontSize="1.5rem" fontWeight={500}>
+              R${price} <span>x10 sem juros</span>
+            </Heading>
             
             {isActive && <p className={styles.Item_description}>{description}</p>}
             {isActive && <button className={styles.buy_item}><img src={cart} alt="cart" /></button>}
-
-            
-            
           </CardFooter>
         </Card>
   )
